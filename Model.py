ae511a9a30b09db700f672d5eeb64703b14a3719
Benson Li <bensonbinbinli@gmail.com>
2019-01-21 16:39:07 -0800

    fixed model

--- Model.py
+++ Model.py
@@ -129,7 +129,7 @@ class Net(FunctionalModule):
                 # nn.ReLU(),
                 )
 
-        self.fc1 = nn.Linear(256,30)
+        self.fc1 = nn.Linear(64,30)
         # self.fc1_batch = nn.BatchNorm1d(30)
         self.fc2 = nn.Linear(30,10)
         # self.fc_last = nn.Linear(100,1)
